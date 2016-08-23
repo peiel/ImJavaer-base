58f4add93fe439f460a4f3274ba8b6bb06f99c9a
Elong <18630149092@139.com>
2016-08-23 14:51:51 +0800

    19  Enum

---
+++ src/main/java/com/elong/enumerated/Burrito.java
@@ -0,0 +1,27 @@
+package com.elong.enumerated;
+
+import static com.elong.enumerated.Spiciness.*;
+
+/**
+ * 创建人 : peierlong
+ * 描述 : 枚举类型可以使用 import static 静态导入
+ */
+public class Burrito {
+
+    Spiciness degree;
+
+    public Burrito(Spiciness degree) {
+        this.degree = degree;
+    }
+
+    @Override
+    public String toString() {
+        return "Burrito is " + degree;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Burrito(NOT));
+        System.out.println(new Burrito(FLAMING));
+        System.out.println(new Burrito(HOT));
+    }
+}
