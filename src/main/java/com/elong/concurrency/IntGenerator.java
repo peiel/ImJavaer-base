9cff413e966e680f25c7cb74d16773f7382fb10d
companyMac <18630149092@139.com>
2016-09-06 16:31:40 +0800

    concurrency end

---
+++ src/main/java/com/elong/concurrency/IntGenerator.java
@@ -0,0 +1,20 @@
+package com.elong.concurrency;
+
+/**
+ * 包名: com.elong.concurrency
+ * 创建人 : Elong
+ * 时间: 16/9/6 下午2:47
+ * 描述 : IntGenerator 抽象类
+ */
+public abstract class IntGenerator {
+
+    private volatile boolean canceled = false;
+    public abstract int next();
+    public void cancel(){
+        canceled = true;
+    }
+    public boolean isCanceled(){
+        return canceled;
+    }
+
+}
