1ea90c1c393d4ecb11ada46726498cf00d2ad429
peierlong <18630149092@139.com>
2016-11-25 17:41:38 +0800

    使用反射调用私有的构造方法

--- src/main/java/com/elong/design/patterns/Singleton.java
+++ src/main/java/com/elong/design/patterns/Singleton.java
@@ -1,10 +1,14 @@
 package com.elong.design.patterns;
 
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Constructor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 包名: com.elong.design.patterns
  * 创建人 : Elong
  * 时间: 2016/11/24 下午4:51
- * 描述 : 证明单例模式可以用 == 比较
+ * 描述 : 单例模式DEMO
  */
 public class Singleton {
 
@@ -24,6 +28,25 @@ public class Singleton {
         //用 == 比较, 返回true
         System.out.println(firstObj == secondObj);
 
+        try {
+            TimeUnit.MILLISECONDS.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /*
+         * 通过反射机制, 可以调用私有的无参构造函数(并非单例了)
+         */
+        try {
+            Constructor<SingletonObject> constructor = SingletonObject.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            SingletonObject invokeObj = constructor.newInstance();
+            invokeObj.setObjName("invokeName");
+            System.out.println(invokeObj.getObjName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
@@ -33,7 +56,10 @@ class SingletonObject {
     private static SingletonObject singletonObject = null;
     private String objName;
 
-    private SingletonObject() {}
+    private SingletonObject() {
+        if (singletonObject != null)
+            throw new IllegalArgumentException("单例模式不能创建新的实例了");
+    }
 
     /*
      * 命名规范:  getInstance返回唯一实例,  newInstance返回不通实例
